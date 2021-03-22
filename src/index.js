a9923cca271132840653e9673c7446cb537c1694
Tammy <tammycwong@gmail.com>
2021-03-22 15:22:58 -0400

    getting this to work

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-function images() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
   fetch(imgUrl)
     .then(response => response(json))
     .then(json => console.log(json));
-}
