088ad4237ee4dfe44eb2bd33c82782e1d74e8464
matthewobrien <matthew.kevin.obrien@gmail.com>
2021-03-22 18:31:32 -0400

    Challenge 1 completed with assistance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 console.log('%c HI', 'color: firebrick')
-document.addEventListener('DOMContentLoaded', function() {
-
-});
-
+// Challenge 1
+  const imgList = document.querySelector("#dog-image-container")
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
   fetch(imgUrl)
-    .then(response => response(json))
-    .then(json => console.log(json));
+    .then(response => response.json())
+    .then(function(imgResponse){
+      let doggyArray = []
+      imgResponse.message.forEach(function(photoLinks){
+        doggyArray.push(photoLinks)
+        console.log(doggyArray)
+      })
+
+      doggyArray.forEach(function(e){
+        let blankLi = document.createElement("li")
+        let blankImg = document.createElement("img")
+        blankImg.src = e
+        blankLi.append(blankImg)
+        imgList.append(blankLi)
+
+      })
+
+
+    })
